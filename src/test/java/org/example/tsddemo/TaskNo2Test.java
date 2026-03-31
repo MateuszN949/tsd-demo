bc16d0f2b639accf6825e7d59763af39910a554e
Bartek Banachowicz <bbanachowicz@egnyte.com>
2026-03-31 16:00:33 +0200

    Fix Task 2

--- src/test/java/org/example/tsddemo/TaskNo2Test.java
+++ src/test/java/org/example/tsddemo/TaskNo2Test.java
@@ -12,7 +12,7 @@ class TaskNo2Test {
     private static final User USER_1 = new User("Andrzej", "Password", "andrzej@example.com", "+48508624734"); // Starts with letter A
     private static final User USER_2 = new User("Anna", "Secret", "anna@example.com", "+48512345678"); // Starts with letter A
     private static final User USER_3 = new User("Bartek", "Password123", "bartek@example.com", "+48765432109");
-    private static final User USER_4 = new User("Zofia", "Passw0rd", "zofia[at]example.com", "+48598765432"); // Invalid email
+    private static final User USER_4 = new User("Zofia", "Passw0rd", "zofia[]example.com", "+48598765432"); // Invalid email
     private static final User USER_5 = new User("Marek", "M@rek", "marek@example.com", "+491512345678"); // Non-Polish number
 
     private static final Set<User> users = Set.of(
@@ -30,7 +30,7 @@ class TaskNo2Test {
         //when
         Collection<User> filteredUsers = sut.filter(users);
         //then
-        assertThat(filteredUsers).containsExactly(USER_3, USER_4, USER_5);
+        assertThat(filteredUsers).doesNotContain(USER_1, USER_2);
     }
 
     @Test
@@ -38,7 +38,7 @@ class TaskNo2Test {
         //when
         Collection<User> filteredUsers = sut.filter(users);
         //then
-        assertThat(filteredUsers).containsExactly(USER_1, USER_2, USER_3, USER_5);
+        assertThat(filteredUsers).doesNotContain(USER_4);
     }
 
     @Test
@@ -46,6 +46,6 @@ class TaskNo2Test {
         //when
         Collection<User> filteredUsers = sut.filter(users);
         //then
-        assertThat(filteredUsers).containsExactly(USER_1, USER_2, USER_3, USER_4);
+        assertThat(filteredUsers).doesNotContain(USER_5);
     }
 }
